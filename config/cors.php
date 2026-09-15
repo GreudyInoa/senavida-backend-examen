935fc9611290574efa7430ef55db09f842d23e0e
GreudyInoa <greudyinoa29@gmail.com>
2026-09-15 12:04:10 -0300

    fix: agregar frontend en produccion al CORS

--- config/cors.php
+++ config/cors.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 return [
 
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://senavida-frontend-examen.wasmer.app'],
 
     'allowed_origins_patterns' => [],
 
